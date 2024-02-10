024ffadca4ed2afd79441bdc8f17f03372db9150
ShakirTechWork <shakir.techwork@gmail.com>
2024-02-10 21:10:00 +0530

    added button to show toast

--- app/src/main/java/com/example/toastapplicaion/MainActivity.kt
+++ app/src/main/java/com/example/toastapplicaion/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.toastapplicaion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.example.toastlibrary.CustomSnackbar
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnShowToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val parentView: View = findViewById(android.R.id.content)
-        val customSnackbar = CustomSnackbar.make(this, parentView, "Custom Snackbar Message", 10)
-        customSnackbar.show()
+        btnShowToast = findViewById(R.id.btn_show_toast)
+
+        btnShowToast.setOnClickListener {
+            val parentView: View = findViewById(android.R.id.content)
+            val customSnackbar = CustomSnackbar.make(this, parentView, "Custom Snackbar Message", 10)
+            customSnackbar.show()
+        }
     }
+
 }
